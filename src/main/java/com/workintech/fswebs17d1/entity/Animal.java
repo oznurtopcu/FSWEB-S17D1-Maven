975451592d1ca8449f9f1c2f5d1985197422445e
oznurtopcu <topcuoznur121@gmail.com>
2025-04-07 23:51:55 +0300

    create animal entity

---
+++ src/main/java/com/workintech/fswebs17d1/entity/Animal.java
@@ -0,0 +1,39 @@
+package com.workintech.fswebs17d1.entity;
+
+import com.workintech.fswebs17d1.controller.AnimalController;
+import org.springframework.stereotype.Component;
+
+
+public class Animal {
+    private int id;
+    private String name;
+
+    public Animal(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
