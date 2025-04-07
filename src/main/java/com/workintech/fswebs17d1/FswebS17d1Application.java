b3c8af22e01264c4865f2cb6bfd1702cdcbf500d
oznurtopcu <topcuoznur121@gmail.com>
2025-04-07 23:51:40 +0300

    test codes added

--- src/main/java/com/workintech/fswebs17d1/FswebS17d1Application.java
+++ src/main/java/com/workintech/fswebs17d1/FswebS17d1Application.java
@@ -2,12 +2,24 @@ package com.workintech.fswebs17d1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FswebS17d1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FswebS17d1Application.class, args);
+		System.out.println("Hello world!");
 	}
+
+//	@Bean
+//	public int getId() {
+//		return 1;
+//	}
+//
+//	@Bean
+//	public String getName() {
+//		return "sincap";
+//	}
 
 }
